5e7aabd898b6a5c6bc1af2571abb075194c92b32
Travis CI User <travis@example.org>
2019-11-26 07:24:05 +0000

    Updated By Travis-CI With Build 111 For Github Pages

---
+++ en.search.min.71223fc3b3a913d952ebf39613dbcd6a56d69601bbdf91cb198ec80e89c172e9.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/interview/flexsearch.min.js');loadScript('/interview/en.search-data.min.642ee1a0613adbf2549c6933af2bc8406b899f1f5a85efab24bc004d07eab9e9.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
